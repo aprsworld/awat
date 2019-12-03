d58a84b51942e3b318c2aaf8e8c3ee53aed6f4f3
David A. Russell <12161175+davarus@users.noreply.github.com>
2019-12-03 15:15:53 -0600

    Added Cellular Signal Strength reading.

--- src/main/java/org/gnarf/bigbrother/gps/GPS.java
+++ src/main/java/org/gnarf/bigbrother/gps/GPS.java
@@ -30,10 +30,10 @@ import org.gnarf.bigbrother.gps.*;
 
 // DAR
 import android.os.*;
+import android.telephony.*;
 // !DAR
 
-public class GPS extends Service
-{
+public class GPS extends Service {
     /* Location manager params */
     LocationManager lm;
     LocListen ll;
@@ -68,195 +68,197 @@ public class GPS extends Service
 
 
     /* DAR Other info */
-	float bat_temp;
-	long uptime;
-	long freespace;
-	int signal;
-	// !DAR
+    float bat_temp;
+    long uptime;
+    long freespace;
+
+    int signal;
+    TelephonyManager tManager;
+    SignalState signal_rcvr;
+    // !DAR
 
     /* Prefs */
     Preferences prefs;
 
-    @Override public void onCreate()
-    {
-		super.onCreate();
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-		StrictMode.setThreadPolicy(policy);
-		System.out.println("GPS Service onCreate.");
+        StrictMode.setThreadPolicy(policy);
+        System.out.println("GPS Service onCreate.");
 
-		setupLocationListener();
+        setupLocationListener();
 
-		setupAlarms();
+        setupAlarms();
 
-		setupBattery();
+        setupBattery();
 
-		/* Get prefs */
-		this.prefs = new Preferences(this);
-		this.prefs.load();
-		reconfigure();
+        setupSignal();
 
-		/* Create formatter */
-		dateformatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
-		dateformatter.setTimeZone(new SimpleTimeZone(0, "UTC"));
+        /* Get prefs */
+        this.prefs = new Preferences(this);
+        this.prefs.load();
+        reconfigure();
 
-		/* Create binder */
-		this.binder = new LocBinder(this);
+        /* Create formatter */
+        dateformatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+        dateformatter.setTimeZone(new SimpleTimeZone(0, "UTC"));
+
+        /* Create binder */
+        this.binder = new LocBinder(this);
 
     }
 
-    @Override public void onDestroy()
-    {
-		super.onDestroy();
-		System.out.println("GPS Service onDestroy.");
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        System.out.println("GPS Service onDestroy.");
 
-		/* Remove alarms and unhook locator */
-		this.am.cancel(this.amintent);
-		this.am.cancel(this.tointent);
-		this.lm.removeUpdates(this.ll);
-		unregisterReceiver(this.recvr);
-		unregisterReceiver(this.bat_rcvr);
-		unregisterReceiver(this.recvTimeout);
+        /* Remove alarms and unhook locator */
+        this.am.cancel(this.amintent);
+        this.am.cancel(this.tointent);
+        this.lm.removeUpdates(this.ll);
+        unregisterReceiver(this.recvr);
+        unregisterReceiver(this.bat_rcvr);
+        unregisterReceiver(this.recvTimeout);
 
-		if (this.notman != null) {
-			/* Remove notification */
-			this.notman.cancelAll();
-		}
+        if (this.notman != null) {
+            /* Remove notification */
+            this.notman.cancelAll();
+        }
     }
 
 
-    @Override public IBinder onBind(Intent i)
-    {
-		return this.binder;
+    @Override
+    public IBinder onBind(Intent i) {
+        return this.binder;
     }
 
-    @Override public boolean onUnbind(Intent i)
-    {
-		this.rpc_if = null;
-		return false;
+    @Override
+    public boolean onUnbind(Intent i) {
+        this.rpc_if = null;
+        return false;
     }
 
 
     private void startLocator()
-		throws IllegalArgumentException
-    {
-		if (this.prefs.provider == 1)
-			this.lm.requestLocationUpdates(this.lm.GPS_PROVIDER, 0, 0,
-										   this.ll);
-		else
-			this.lm.requestLocationUpdates(this.lm.NETWORK_PROVIDER, 0, 0,
-										   this.ll);
+            throws IllegalArgumentException {
+        if (this.prefs.provider == 1)
+            this.lm.requestLocationUpdates(this.lm.GPS_PROVIDER, 0, 0,
+                    this.ll);
+        else
+            this.lm.requestLocationUpdates(this.lm.NETWORK_PROVIDER, 0, 0,
+                    this.ll);
     }
 
-    public void triggerUpdate()
-    {
-		if (this.prefs.continous_mode) {
-			/* If we are in continous mode just send last location */
-			locationUpdate();
-		}
+    public void triggerUpdate() {
+        if (this.prefs.continous_mode) {
+            /* If we are in continous mode just send last location */
+            locationUpdate();
+        } else {
+            /* Polling mode */
 
-		else {
-			/* Polling mode */
+            this.lm.removeUpdates(this.ll);
 
-			this.lm.removeUpdates(this.ll);
+            try {
+                startLocator();
+            } catch (IllegalArgumentException e) {
+                System.out.println("BigBrotherGPS: " + e.toString());
+                return;
+            }
 
-			try {
-				startLocator();
-			}
-			catch (IllegalArgumentException e) {
-				System.out.println("BigBrotherGPS: "+e.toString());
-				return;
-			}
-
-			/* Start timeout alarm */
-			this.timeout = System.currentTimeMillis();
-			this.timeout += this.prefs.gps_timeout;
-			this.timeout += 100; /* delay a bit to avoid a race */
-			this.am.setRepeating(this.am.RTC_WAKEUP, this.timeout,
-								 this.prefs.gps_timeout, this.tointent);
-			if (this.prefs.provider == 1)
-				this.twiceTimeout = true;
-			else
-				this.twiceTimeout = false;
-		}
+            /* Start timeout alarm */
+            this.timeout = System.currentTimeMillis();
+            this.timeout += this.prefs.gps_timeout;
+            this.timeout += 100; /* delay a bit to avoid a race */
+            this.am.setRepeating(this.am.RTC_WAKEUP, this.timeout,
+                    this.prefs.gps_timeout, this.tointent);
+            if (this.prefs.provider == 1)
+                this.twiceTimeout = true;
+            else
+                this.twiceTimeout = false;
+        }
     }
 
-    private void setupLocationListener()
-    {
-		/* Set up the position listener */
-		this.ll = new LocListen();
-		this.lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    private void setupLocationListener() {
+        /* Set up the position listener */
+        this.ll = new LocListen();
+        this.lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
-    private void setupNotif()
-    {
-		if (this.prefs.show_in_notif_bar) {
-			/* Show state in notif bar */
+    private void setupNotif() {
+        if (this.prefs.show_in_notif_bar) {
+            /* Show state in notif bar */
 
-			if (this.notman != null) {
-				/* Already set up */
-				return;
-			}
+            if (this.notman != null) {
+                /* Already set up */
+                return;
+            }
 
-			/* Set up a persistent notification */
-			this.notman = (NotificationManager)
-				getSystemService(Context.NOTIFICATION_SERVICE);
-			this.notif = new Notification(R.drawable.notif_icon,
-										  "BigBrother GPS Waiting for location",
-										  System.currentTimeMillis());
-			this.notif.flags = notif.FLAG_ONGOING_EVENT;
-			this.notintent =
-				PendingIntent.getActivity(this, 0,
-										  new Intent(this, BigBrotherGPS.class),0);
-			this.notif.setLatestEventInfo(this, getString(R.string.app_name),
-										  "Waiting for initial location",
-										  notintent);
-			this.notman.notify(1, notif);
-			if (this.prefs.continous_mode) {
+            /* Set up a persistent notification */
+            this.notman = (NotificationManager)
+                    getSystemService(Context.NOTIFICATION_SERVICE);
+            this.notif = new Notification(R.drawable.notif_icon,
+                    "BigBrother GPS Waiting for location",
+                    System.currentTimeMillis());
+            this.notif.flags = notif.FLAG_ONGOING_EVENT;
+            this.notintent =
+                    PendingIntent.getActivity(this, 0,
+                            new Intent(this, BigBrotherGPS.class), 0);
+            this.notif.setLatestEventInfo(this, getString(R.string.app_name),
+                    "Waiting for initial location",
+                    notintent);
+            this.notman.notify(1, notif);
+            if (this.prefs.continous_mode) {
                 this.startForeground(1, this.notif);
             }
-		} else {
-			if (this.notman != null) {
-				/* Remove notification */
+        } else {
+            if (this.notman != null) {
+                /* Remove notification */
                 this.stopForeground(true);
-				this.notman.cancelAll();
-				this.notman = null;
-			}
-		}
+                this.notman.cancelAll();
+                this.notman = null;
+            }
+        }
     }
 
-    private void setupAlarms()
+    private void setupAlarms() {
+        /* Prepare alarm manager */
+        this.am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        /* Setup location update alarm */
+        this.recvr = new LocAlarm();
+        registerReceiver(this.recvr,
+                new IntentFilter(LocAlarm.class.toString()),
+                null, null);
+        Intent i = new Intent(LocAlarm.class.toString());
+        this.amintent = PendingIntent.getBroadcast(this, 0, i, 0);
+
+        /* Setup timeout alarm */
+        this.recvTimeout = new LocTimeout();
+        registerReceiver(this.recvTimeout,
+                new IntentFilter(LocTimeout.class.toString()),
+                null, null);
+        i = new Intent(LocTimeout.class.toString());
+        this.tointent = PendingIntent.getBroadcast(this, 0, i, 0);
+
+    }
+
+    private void setupBattery() {
+        /* Setup location update alarm */
+        this.bat_rcvr = new BatteryState();
+        registerReceiver(this.bat_rcvr,
+                new IntentFilter(Intent.ACTION_BATTERY_CHANGED),
+                null, null);
+    }
+
+    private void setupSignal()
     {
-		/* Prepare alarm manager */
-		this.am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-
-		/* Setup location update alarm */
-		this.recvr = new LocAlarm();
-		registerReceiver(this.recvr,
-						 new IntentFilter(LocAlarm.class.toString()),
-						 null, null);
-		Intent i = new Intent(LocAlarm.class.toString());
-		this.amintent = PendingIntent.getBroadcast(this, 0, i, 0);
-
-		/* Setup timeout alarm */
-		this.recvTimeout = new LocTimeout();
-		registerReceiver(this.recvTimeout,
-						 new IntentFilter(LocTimeout.class.toString()),
-						 null, null);
-		i = new Intent(LocTimeout.class.toString());
-		this.tointent = PendingIntent.getBroadcast(this, 0, i, 0);
-
+        this.tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        this.signal_rcvr = new SignalState();
+        this.tManager.listen(this.signal_rcvr, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
     }
-
-    private void setupBattery()
-    {
-		/* Setup location update alarm */
-		this.bat_rcvr = new BatteryState();
-		registerReceiver(this.bat_rcvr,
-						 new IntentFilter(Intent.ACTION_BATTERY_CHANGED),
-						 null, null);
-    }
-
 
     public void doTimeout()
     {
@@ -593,6 +595,13 @@ public class GPS extends Service
 			System.out.printf("BigBrotherGPS: Battery temp: %f%%\n", GPS.this.bat_temp);
 			// !DAR
 		}
+    }
+
+    class SignalState extends PhoneStateListener {
+        @Override public void onSignalStrengthsChanged(SignalStrength strength) {
+            super.onSignalStrengthsChanged(strength);
+            GPS.this.signal = strength.getGsmSignalStrength();
+        }
     }
 
 
