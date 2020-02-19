8daa4d0f97c255e8500b99b619130019e848f050
David A. Russell <12161175+davarus@users.noreply.github.com>
2020-02-19 14:15:48 -0600

    Working on warnings (Issue #2).
    Updated Preferences to use PreferenceFragment as old system is depreciated.

--- src/main/java/com/aprsworld/awat/gps/PreferencesActivity.java
+++ src/main/java/com/aprsworld/awat/gps/PreferencesActivity.java
@@ -2,12 +2,22 @@ package com.aprsworld.awat.gps;
 
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
 /* The preference displaying and poking around activity */
 public class PreferencesActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
+    }
+
+    public static class PreferencesFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
