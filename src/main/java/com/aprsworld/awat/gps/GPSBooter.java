4ef1add331e1c3825039966f0c57bff157c480e0
David A. Russell <12161175+davarus@users.noreply.github.com>
2020-02-19 14:26:53 -0600

    Working on warnings (Issue #2).
    Bugfix on intent string comparison.

--- src/main/java/com/aprsworld/awat/gps/GPSBooter.java
+++ src/main/java/com/aprsworld/awat/gps/GPSBooter.java
@@ -13,7 +13,7 @@ public class GPSBooter extends BroadcastReceiver {
         Preferences prefs = new Preferences(ctx);
         prefs.load();
 
-        if (!i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        if (i.getAction() == null || !i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
         }
 
