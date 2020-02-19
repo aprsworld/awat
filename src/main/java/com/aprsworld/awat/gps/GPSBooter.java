81f876b3e82e6256e7b60bdacdff0c4474bbb695
David A. Russell <12161175+davarus@users.noreply.github.com>
2020-02-19 14:00:56 -0600

    Working on warnings (Issue #2).

--- src/main/java/com/aprsworld/awat/gps/GPSBooter.java
+++ src/main/java/com/aprsworld/awat/gps/GPSBooter.java
@@ -13,6 +13,10 @@ public class GPSBooter extends BroadcastReceiver {
         Preferences prefs = new Preferences(ctx);
         prefs.load();
 
+        if (i.getAction() != Intent.ACTION_BOOT_COMPLETED) {
+            return;
+        }
+
         if (prefs.start_on_boot) {
             Intent svc_i = new Intent(ctx, GPS.class);
             ComponentName svc = ctx.startService(svc_i);
