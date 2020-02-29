685eb2d3ee7c4ea3e7814dfb3b69dc37031915f6
David A. Russell <12161175+davarus@users.noreply.github.com>
2020-02-28 18:14:00 -0600

    Added Build Info to Changelog Screen.

--- src/main/java/com/aprsworld/awat/gps/Changelog.java
+++ src/main/java/com/aprsworld/awat/gps/Changelog.java
@@ -2,6 +2,13 @@ package com.aprsworld.awat.gps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 
 public class Changelog extends Activity {
@@ -14,6 +21,21 @@ public class Changelog extends Activity {
 
         /* Create the UI */
         setContentView(R.layout.changelog);
+
+        /* Populate Build Date */
+        TextView tv;
+        tv = findViewById(R.id.changelog_build_versioncode);
+        tv.setText(String.format(Locale.US, "%d", BuildConfig.VERSION_CODE));
+        tv = findViewById(R.id.changelog_build_version);
+        tv.setText(BuildConfig.VERSION_NAME);
+        tv = findViewById(R.id.changelog_build_date);
+        tv.setText((new SimpleDateFormat("MM/DD/YYYY HH:mm:ss.SS", Locale.US)).format(BuildConfig.BUILD_TIME));
+        tv = findViewById(R.id.changelog_build_type);
+        tv.setText(BuildConfig.BUILD_TYPE);
+        tv = findViewById(R.id.changelog_build_git_hash);
+        tv.setText(BuildConfig.BUILD_GIT_HASH);
+        tv = findViewById(R.id.changelog_build_git_clean);
+        tv.setText(BuildConfig.BUILD_GIT_CLEAN ? "True" : "False");
     }
 
 
